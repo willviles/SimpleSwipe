b50238becebbadfea4f919d7a0493441dc1e7867
willviles <will@willviles.com>
2015-04-07 17:52:59 +0100

    Basic recognition of swiping directions using Hammer.js

--- dist/simple-swipe.js
+++ dist/simple-swipe.js
@@ -12,7 +12,7 @@ Author URI: http://vil.es/
   var SimpleSwipe = function( elem, options ){
       this.elem = elem;
       this.$elem = $(elem);
-      this.$cards = this.$elem.find('li');
+      this.$currentCard = this.$elem.find('li:first-of-type');
       this.options = $.extend({
         // Defaults
         something: false
@@ -29,9 +29,7 @@ Author URI: http://vil.es/
 
     init: function() {
 
-
-
-      console.log('SimpleSwipe is being initiated');
+      this.card.setup.call(this);
 
       return this;
     },
@@ -41,14 +39,98 @@ Author URI: http://vil.es/
     ///////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////
 
-    registerCards: function() {
+    card: {
+
+      // Setup card
+      ///////////////////////////////////////////////////////
+
+      setup: function() {
+        // Get first card in the stack
+        $card = this.$elem.find('li:first-of-type');
+        this.$currentCard = $card;
+
+        // Setup Hammer.js handler
+        var card = new Hammer($card[0]),
+            availableDirections = this.card.getAvailableDirections.call(this),
+            that = this;
+
+        // Set pan options
+        card.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+        card.get('swipe').set({ direction: Hammer.DIRECTION_ALL, threshold: 20 });
+
+        // On a successful swipe
+        card.on('swipe', function(event) {
+          that.card.handleSwipe.call(that, event, availableDirections);
+        });
+
+        card.on('panstart', function(event) {
+          console.log('PAN START');
+        });
+
+        card.on('panmove', function(event) {
+          that.card.drag.call(that, event.deltaX, event.deltaY);
+        });
+
+        card.on('panend', function(event) {
+          that.card.springBack.call(that);
+        });
+      },
+
+      // Set Directions
+      getAvailableDirections: function() {
+        var card = this.$currentCard,
+            defaultDirections = ['up', 'right', 'down', 'left'],
+            availableDirections = [];
+
+        $.each(defaultDirections, function(i, direction) {
+          if (card.data('swipe-' + direction) == true) {
+            availableDirections.push(direction);
+          };
+        });
+
+        return availableDirections;
+      },
+
+      // Drag card
+      ///////////////////////////////////////////////////////
+
+      drag: function(x, y) {
+        this.$currentCard.css({
+         '-webkit-transform': 'translate3d(0, 0, 0) translate(' + x + 'px, ' + y + 'px)'
+        });
+      },
+
+      // Spring back
+      ///////////////////////////////////////////////////////
+      springBack: function() {
+        this.$currentCard.css({
+         '-webkit-transform': 'translate3d(0, 0, 0) translate(0px, 0px)'
+        });
+      },
+
+      // Handle Swipe
+      ///////////////////////////////////////////////////////
+      handleSwipe: function(event, availableDirections) {
+
+        var directionKey = event.direction,
+            directionKeys = {'8': 'up', '4': 'right', '2': 'left', '16': 'down'},
+            direction = directionKeys[directionKey];
+
+        alert(direction + ' swiped');
+
+        if ($.inArray(direction, availableDirections) !== -1) {
+          console.log(direction + ' swipe is an available direction.');
+        };
+      },
+
+      // Move onto next card
+      ///////////////////////////////////////////////////////
+
+      next: function() {
+        console.log('Awesome, we have successfully swiped');
+      }
 
     },
-
-    // Handle touch
-    ///////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////
 
 
   }; // End of all functions
